b27ead9c6e1e77cadef008439775a3fa51bcf38b
Robert May <rob@themayfamily.me.uk>
2015-10-16 22:25:40 +0100

    Initial commit for v0.0.2
    Tidy up documentaiton;  Bring up the bluetooth stack ourselves, rather
    than using the DAL implementation - should give us more memory, and
    results in everything being in a known state; Correct use of Long UUIDs
    for the discovered characteristics; Perform characteristic discovery
    against all services in preparation for using more of them; Improve
    response of button press.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,14 @@
+#include "MicroBitCustomConfig.h"
 #include "MicroBit.h"
 #include "ble/DiscoveredCharacteristic.h"
 #include "ble/DiscoveredService.h"
 
 #if YOTTA_CFG_MICROBIT_S130 != 1
 #error This code *only* works with the Nordik S130 softdevice
+#endif
+
+#if CONFIG_ENABLED(MICROBIT_DBG)
+#error use of the serial port by MICROBIT_DBG clashes with our use of the serial port - not uspported
 #endif
 
 #define MY_DEBUG 1
@@ -22,13 +27,8 @@
 #define SPUB_EVENT(type, reason) uBit.serial.printf ("P %s %s 0x%04x:0x%04x\n", device_id, "mb\\evtsvc", type, reason)
 
 /* NOTES
-* XXX Why is the stack using short UUIDs for our (presumably) private services and characteristics?
 * XXX Would be great to have some code review by someone who knows what they're talking about - I've cargo culted most of this
 */
-
-#define MicrobitEventServiceShortUUID        0x93AF /* XXX Not sure why the stack is using the short UUID, but it appears to be */
-#define MicrobitEventCharacteristicShortUUID 0x9775 /* XXX Not sure why the stack is using the short UUID, but it appears to be */
-#define ClientEventCharacteristicShortUUID   0x5404 /* XXX Not sure why the stack is using the short UUID, but it appears to be */
 
 char *device_id = NULL; 
 DiscoveredCharacteristic microbitEventCharacteristic;
@@ -77,7 +77,7 @@ void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params) {
                 }
             }
         }
-        pos = pos + adlen;
+        pos = pos + adlen + 1;
     }
 }
 
@@ -106,11 +106,11 @@ void discoveryTerminationCallback(Gap::Handle_t connectionHandle) {
 }
 
 void characteristicDiscoveryCallback(const DiscoveredCharacteristic *characteristicP) {
-    if (characteristicP->getUUID().getShortUUID() == MicrobitEventCharacteristicShortUUID) { 
+    if (characteristicP->getUUID() == UUID(MicroBitEventServiceMicroBitEventCharacteristicUUID)) { 
         microbitEventCharacteristic      = *characteristicP;
         foundMicrobitEventCharacteristic = true;
     }
-    if (characteristicP->getUUID().getShortUUID() == ClientEventCharacteristicShortUUID) { 
+    if (characteristicP->getUUID() ==  UUID(MicroBitEventServiceClientEventCharacteristicUUID)) { 
         clientEventCharacteristic      = *characteristicP;
         foundClientEventCharacteristic = true;
     }
@@ -121,14 +121,13 @@ void connectionCallback(const Gap::ConnectionCallbackParams_t *params) {
     if (params->role == Gap::CENTRAL) {
         uBit.display.print('D'); /* Discovery phase */
         uBit.ble->gattClient().onServiceDiscoveryTermination(discoveryTerminationCallback);
-        uBit.ble->gattClient().launchServiceDiscovery(params->handle, NULL ,characteristicDiscoveryCallback, MicrobitEventServiceShortUUID);
+        uBit.ble->gattClient().launchServiceDiscovery(params->handle, NULL ,characteristicDiscoveryCallback);
     }
 }
 
 void disconnectionCallback(Gap::Handle_t handle, Gap::DisconnectionReason_t reason) {
     (void) handle; /* -Wunused-parameter */
     (void) reason; /* -Wunused-parameter */
-
     /* re-start Scan */
     start_ad_scan();
 }
@@ -156,7 +155,6 @@ void process_cmd(char* s) {
     uint8_t state = 0;
     char *parts[CMD_MAX] = { NULL };
 
-    /* SD("Processing command '%s'", s); */
     while((s <= e) && (state < CMD_MAX)) {
         p = s;
         while((s <= e) && (*s != ' ')) { ++s; }
@@ -230,17 +228,18 @@ void serialRxCallback () {
 }
 
 void app_main() {
-    uBit.ble->gap().stopAdvertising();                      /* stop the advertising that the DAL initiates */
-    uBit.ble->gap().onDisconnection(disconnectionCallback); /* Overwrites the one that the DAL installs - stop re-starting advertising */
+    uBit.ble = new BLEDevice();
+    uBit.ble->init();
+    uBit.ble->gap().onDisconnection(disconnectionCallback);
     uBit.ble->gap().onConnection(connectionCallback);
     uBit.ble->gattClient().onHVX(hvxCallback);
     uBit.serial.attach(serialRxCallback);
 
     /* Wait until we have a device id to connect to
-     * OR press (and hold for up to one sec) button A to connect to the first found micro:bit */
+     * OR press button A to connect to the first found micro:bit */
     uBit.display.print('?');
     while(!device_id && !uBit.buttonA.isPressed()) {
-        uBit.sleep(1000); /* should we reduce this sleep to make the button response better? */
+        uBit.sleep(20);
     }
 
     start_ad_scan(); /* start the scan/discover/connect sequence */
